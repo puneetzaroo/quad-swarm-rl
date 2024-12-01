1a946348c4efbb0944ebbbe24aa35cc49d8706aa
Puneet Zaroo <pzaroo@netflix.com>
2024-11-30 22:32:34 -0800

    Change in dependencies and number of training steps.

--- train.sh
+++ train.sh
@@ -1,2 +1,3 @@
 python -m sample_factory.launcher.run --run=swarm_rl.runs.single_quad.single_quad \
---backend=processes --max_parallel=1 --pause_between=1 --experiments_per_gpu=1 --num_gpus=1
+--backend=processes --max_parallel=8 --pause_between=1 --experiments_per_gpu=1 --num_gpus=4
+--train_for_env_steps=1000000 
